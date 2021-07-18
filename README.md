823c8c576f84350e01312b2a6e90c5bd5af4afe4
tsubasa1122 <nakanot@open8.com>
2021-07-19 00:30:47 +0900

    Linter系のセットアップ

---
+++ README.md
@@ -0,0 +1,9 @@
+# プラハチャレンジ DDD 課題用のリポジトリ
+
+### Install Dependencies:
+
+```
+yarn install
+```
+
+### Create Database
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import { PrismaClient } from '@prisma/client';
+import express from "express";
+
+// const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
