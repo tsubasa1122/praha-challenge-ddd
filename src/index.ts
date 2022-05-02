d99337806179895c1225103fcdb7fa0e240c9a96
tsubasa <tsubasan1122@gmail.com>
2022-05-02 13:32:20 +0900

    不要な条件分岐削除

--- src/index.ts
+++ src/index.ts
@@ -15,39 +15,37 @@ app.use(express.json())
 // CORSを許可する 今回は雑に全てのオリジンを許可する
 app.use(cors())
 
-if (serviceAccount) {
-  const firebase = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    databaseURL: 'https://clean-bindery-220507.firebaseio.com',
-  })
-  firebase.auth()
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: 'https://clean-bindery-220507.firebaseio.com',
+})
+firebase.auth()
 
-  // routers
-  app.use('/api/participants', Routes.participantsRouter)
-  app.use('/api/tasks', Routes.tasksRouter)
+// routers
+app.use('/api/participants', Routes.participantsRouter)
+app.use('/api/tasks', Routes.tasksRouter)
 
-  // helth check
-  app.get('/api/hc', (async (req, res) => {
-    // 愚直に認可処理を入れたけど、共通化したい
-    const idToken = req.header('Authorization')
+// helth check
+app.get('/api/hc', (async (req, res) => {
+  // 愚直に認可処理を入れたけど、共通化したい
+  const idToken = req.header('Authorization')
 
-    if (idToken) {
-      const currentUser = await getAuth().verifyIdToken(idToken)
+  if (idToken) {
+    const currentUser = await getAuth().verifyIdToken(idToken)
 
-      // ユーザーがいたらユーザー名を返す
-      if (currentUser.name) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        res.status(200).json({ name: currentUser.name })
-        return
-      }
-
-      res.status(401).json({ message: '不正なリクエストです。' })
+    // ユーザーがいたらユーザー名を返す
+    if (currentUser.name) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      res.status(200).json({ name: currentUser.name })
       return
     }
 
     res.status(401).json({ message: '不正なリクエストです。' })
-  }) as RequestHandler)
+    return
+  }
 
-  // start up server
-  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
-}
+  res.status(401).json({ message: '不正なリクエストです。' })
+}) as RequestHandler)
+
+// start up server
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
