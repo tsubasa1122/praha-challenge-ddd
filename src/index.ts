35024c2c0cbdaae715c8e355e19db153dcc79a61
tsubasa <tsubasan1122@gmail.com>
2022-07-23 16:06:20 +0900

    DBのClientをRouteの引数で受け取るようにした

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 // import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import * as Routes from './routes'
 
-// const prisma = new PrismaClient();
 const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 
+const prisma = new PrismaClient()
+
 // routers
-app.use('/api/participants', Routes.participantsRouter)
-app.use('/api/tasks', Routes.tasksRouter)
+app.use('/api/participants', Routes.participantsRouter(prisma))
+// app.use('/api/tasks', Routes.tasksRouter)
 
 // health check
 app.get('/api/hc', (_req, res) => res.send('ok!!'))
